7fbf20175550149e9e230165a84d199e88ee80b9
Ashley Coles <ashley@acoles.co.uk>
2022-06-22 10:02:00 +0100

    Adding check to install script to make sure it's not already installed

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 
-mv phpunit.sh ~
-chmod +x ~/phpunit.sh
-echo "alias phpunit='~/phpunit.sh'" >> ~/.zshrc
-source ~/.zshrc
+FILE=~/phpunit.sh
+
+if [ -f "$FILE" ]; then
+  echo "Looks like you've already ran this install script!"
+else
+  mv phpunit.sh ~
+  chmod +x ~/phpunit.sh
+  echo "alias phpunit='~/phpunit.sh'" >> ~/.zshrc
+  source ~/.zshrc
+fi
