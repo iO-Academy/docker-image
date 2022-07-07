f3e4eff690182520c28c3c962317e31a52bb87c2
Ashley Coles <ashley@acoles.co.uk>
2022-07-07 09:56:08 +0100

    Swithing install script to copy scripts instead of move

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ PHPUNIT_FILE=~/phpunit.sh
 if [ -f "$PHPUNIT_FILE" ]; then
   echo "Looks like you've already ran this installer"
 else
-  mv phpunit.sh ~
+  cp phpunit.sh ~
   chmod +x ~/phpunit.sh
   echo "alias phpunit='~/phpunit.sh'" >> ~/.zshrc
   source ~/.zshrc
@@ -16,7 +16,7 @@ COMPOSER_FILE=~/composer.sh
 if [ -f "$COMPOSER_FILE" ]; then
   echo "Looks like you've already ran this installer"
 else
-  mv composer.sh ~
+  cp composer.sh ~
   chmod +x ~/composer.sh
   echo "alias composer='~/composer.sh'" >> ~/.zshrc
   source ~/.zshrc
