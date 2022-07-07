b7449609c15bff1b12943cfa5c653f88986f2de2
Ashley Coles <ashley@acoles.co.uk>
2022-07-07 11:40:35 +0100

    Fixing check for html folder

--- composer.sh
+++ composer.sh
@@ -3,7 +3,7 @@
 WD=$(pwd)
 
 DOCKER_PATH=$(sed 's/^.*\(html.*\).*$/\1/' <<<"$WD")
-FOLDER_CHECK='docker-image/html'
+FOLDER_CHECK='html'
 
 if [[ "$DOCKER_PATH" == *"$FOLDER_CHECK"* ]]; then
   docker exec -ti -w "/var/www/$DOCKER_PATH" academy-php composer "$1"
--- phpunit.sh
+++ phpunit.sh
@@ -3,7 +3,7 @@
 WD=$(pwd)
 
 DOCKER_PATH=$(sed 's/^.*\(html.*\).*$/\1/' <<<"$WD")
-FOLDER_CHECK='docker-image/html'
+FOLDER_CHECK='html'
 
 if [[ "$DOCKER_PATH" == *"$FOLDER_CHECK"* ]]; then
   docker exec -w "/var/www/$DOCKER_PATH"  academy-php phpunit "$1"
