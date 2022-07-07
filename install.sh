535d548364e79eda061b00ce0adc483f8e3edf1c
Ashley Coles <ashley@acoles.co.uk>
2022-07-07 11:55:34 +0100

    Improving install script

--- install.sh
+++ install.sh
@@ -2,33 +2,43 @@
 
 PHPUNIT_FILE=~/phpunit.sh
 
+INSTALLED=false
+
 if [ -f "$PHPUNIT_FILE" ]; then
-  echo "Looks like you've already ran this installer"
+  echo "Looks like you already have the phpunit script installed"
 else
   cp phpunit.sh ~
   chmod +x ~/phpunit.sh
   echo "alias phpunit='~/phpunit.sh'" >> ~/.zshrc
-  source ~/.zshrc
+  INSTALLED=true
 fi
 
 COMPOSER_FILE=~/composer.sh
 
 if [ -f "$COMPOSER_FILE" ]; then
-  echo "Looks like you've already ran this installer"
+  echo "Looks like you already have the composer script installed"
 else
   cp composer.sh ~
   chmod +x ~/composer.sh
   echo "alias composer='~/composer.sh'" >> ~/.zshrc
-  source ~/.zshrc
+  INSTALLED=true
 fi
 
 PHP_FILE=~/php.sh
 
 if [ -f "$PHP_FILE" ]; then
-  echo "Looks like you've already ran this installer"
+  echo "Looks like you already have the php script installed"
 else
   cp php.sh ~
   chmod +x ~/php.sh
   echo "alias php='~/php.sh'" >> ~/.zshrc
-  source ~/.zshrc
+  INSTALLED=true
+fi
+
+if $INSTALLED; then
+    source ~/.zshrc
+    clear
+    echo "Install completed"
+else
+  echo "No install needed"
 fi
