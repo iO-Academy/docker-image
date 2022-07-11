b2bd65336e0d6587788c1a77774e5e9b69c8c462
Ashley Coles <ashley@acoles.co.uk>
2022-07-11 12:49:33 +0100

    Tidying script

--- install.sh
+++ install.sh
@@ -18,7 +18,32 @@ RED='\033[0;31m'
 BOLD_GREEN='\033[1;32m'
 BOLD_RED='\033[1;31m'
 BOLD_YELLOW='\033[1;33m'
+
 COLOUR_OFF='\033[0m'
+
+echo_success () {
+  if (( $# == 2 )) && (($2 == true)) then
+    echo "${BOLD_GREEN}${1}${COLOUR_OFF}"
+  else
+    echo "${GREEN}${1}${COLOUR_OFF}"
+  fi
+}
+
+echo_warning () {
+  if (( $# == 2 )) && (($2 == true)) then
+    echo "${BOLD_YELLOW}${1}${COLOUR_OFF}"
+  else
+    echo "${YELLOW}${1}${COLOUR_OFF}"
+  fi
+}
+
+echo_fail () {
+  if (( $# == 2 )) && (($2 == true)) then
+    echo "${BOLD_RED}${1}${COLOUR_OFF}"
+  else
+    echo "${RED}${1}${COLOUR_OFF}"
+  fi
+}
 
 echo "PHPUnit: Attempting to install the phpunit.sh helper script"
 
@@ -26,7 +51,7 @@ PHPUNIT_FILE=~/phpunit.sh
 
 # Checks to see if the file is already in ~
 if [ -f "$PHPUNIT_FILE" ]; then
-  echo "${YELLOW}PHPUnit: Looks like you already have the phpunit.sh script installed${COLOUR_OFF}"
+  echo_warning "PHPUnit: Looks like you already have the phpunit.sh script installed"
 else
   # Move the file to the home directory
   cp phpunit.sh ~
@@ -37,10 +62,10 @@ else
     # Add an alias to .zshrc
     echo "alias phpunit='~/phpunit.sh'" >> ~/.zshrc
     INSTALLED=true
-    echo -e "${GREEN}PHPUnit: phpunit.sh installed successfully${COLOUR_OFF}"
+    echo_success "PHPUnit: phpunit.sh installed successfully"
   else
     ERROR=true
-    echo "${RED}PHPUnit: Error unable to make phpunit.sh executable${COLOUR_OFF}"
+    echo_fail "PHPUnit: Error unable to make phpunit.sh executable"
   fi
 fi
 
@@ -49,17 +74,17 @@ echo "Composer: Attempting to install the composer.sh helper script"
 COMPOSER_FILE=~/composer.sh
 
 if [ -f "$COMPOSER_FILE" ]; then
-  echo "${YELLOW}Composer: Looks like you already have the composer.sh script installed${COLOUR_OFF}"
+  echo_warning "Composer: Looks like you already have the composer.sh script installed"
 else
   cp composer.sh ~
   chmod +x ~/composer.sh
   if [ -x "$COMPOSER_FILE" ]; then
     echo "alias composer='~/composer.sh'" >> ~/.zshrc
     INSTALLED=true
-    echo "${GREEN}Composer: composer.sh installed successfully${COLOUR_OFF}"
+    echo_success "Composer: composer.sh installed successfully"
   else
     ERROR=true
-    echo "${RED}Composer: Error unable to make composer.sh executable${COLOUR_OFF}"
+    echo_fail "Composer: Error unable to make composer.sh executable"
   fi
 fi
 
@@ -68,17 +93,17 @@ echo "PHP: Attempting to install the php.sh helper script"
 PHP_FILE=~/php.sh
 
 if [ -f "$PHP_FILE" ]; then
-  echo "${YELLOW}PHP: Looks like you already have the php.sh script installed${COLOUR_OFF}"
+  echo_warning "PHP: Looks like you already have the php.sh script installed"
 else
   cp php.sh ~
   chmod +x ~/php.sh
   if [ -x "$PHP_FILE" ]; then
     echo "alias php='~/php.sh'" >> ~/.zshrc
     INSTALLED=true
-    echo "${GREEN}PHP: php.sh installed successfully${COLOUR_OFF}"
+    echo_success "PHP: php.sh installed successfully"
   else
     ERROR=true
-    echo "${RED}PHP: Error unable to make php.sh executable${COLOUR_OFF}"
+    echo_fail "PHP: Error unable to make php.sh executable"
   fi
 fi
 
@@ -86,11 +111,11 @@ fi
 if $INSTALLED; then
     echo "Sourcing .zshrc"
     source ~/.zshrc
-    echo "${BOLD_GREEN}Install complete${COLOUR_OFF}"
+    echo_success "Install complete" true
 else
   if $ERROR; then
-    echo "${BOLD_RED}Error: Install failed${COLOUR_OFF}"
+    echo_fail "Error: Install failed" true
   else
-    echo "${BOLD_YELLOW}Install already complete${COLOUR_OFF}"
+    echo_success "Install already complete" true
   fi
 fi
