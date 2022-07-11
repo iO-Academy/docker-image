44ee2b1339f58c9d680e2c1ee33ab286a9059c94
Ashley Coles <ashley@acoles.co.uk>
2022-07-11 10:05:44 +0100

    Adding comments, improving output and making more robust

--- composer.sh
+++ composer.sh
@@ -1,12 +1,17 @@
 #!/bin/zsh
 
+# Grab the working directory
 WD=$(pwd)
 
+# Remove everything from the path before 'html'
 DOCKER_PATH=$(sed 's/^.*\(html.*\).*$/\1/' <<<"$WD")
 FOLDER_CHECK='html'
 
+# Make sure the current working directory is actually in docker
 if [[ "$DOCKER_PATH" == *"$FOLDER_CHECK"* ]]; then
+  # Execute the command through docker
   docker exec -ti -w "/var/www/$DOCKER_PATH" academy-php composer "$1"
 else
+  # Otherwise we're outside the html folder, so run the original command
   composer "$1"
 fi
--- install.sh
+++ install.sh
@@ -1,44 +1,96 @@
 #!/bin/zsh
 
+# This file is used to install the docker 'helper' scripts for
+# PHP, PHPUnit and Composer, allowing us to run them via docker
+# without having to manually use docker exec commands.
+
+# It firstly checks to make sure they aren't each already installed
+# then copies them into ~, chmods them and adds an alias to ~/.zshrc
+# to run the helper scripts instead of your locally installed versions.
+
+# Used to work out whether or not we need to source ~/.zshrc
+INSTALLED=false
+ERROR=false
+
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+BOLD_GREEN='\033[1;32m'
+BOLD_RED='\033[1;31m'
+BOLD_YELLOW='\033[1;33m'
+COLOUR_OFF='\033[0m'
+
+echo "PHPUnit: Attempting to install the phpunit.sh helper script"
+
 PHPUNIT_FILE=~/phpunit.sh
 
-INSTALLED=false
-
+# Checks to see if the file is already in ~
 if [ -f "$PHPUNIT_FILE" ]; then
-  echo "Looks like you already have the phpunit script installed"
+  echo "${YELLOW}PHPUnit: Looks like you already have the phpunit.sh script installed${COLOUR_OFF}"
 else
+  # Move the file to the home directory
   cp phpunit.sh ~
+  # Make the sh file executable
   chmod +x ~/phpunit.sh
-  echo "alias phpunit='~/phpunit.sh'" >> ~/.zshrc
-  INSTALLED=true
+  # Did chmod work?
+  if [ -x "$PHPUNIT_FILE" ]; then
+    # Add an alias to .zshrc
+    echo "alias phpunit='~/phpunit.sh'" >> ~/.zshrc
+    INSTALLED=true
+    echo -e "${GREEN}PHPUnit: phpunit.sh installed successfully${COLOUR_OFF}"
+  else
+    ERROR=true
+    echo "${RED}PHPUnit: Error unable to make phpunit.sh executable${COLOUR_OFF}"
+  fi
 fi
+
+echo "Composer: Attempting to install the composer.sh helper script"
 
 COMPOSER_FILE=~/composer.sh
 
 if [ -f "$COMPOSER_FILE" ]; then
-  echo "Looks like you already have the composer script installed"
+  echo "${YELLOW}Composer: Looks like you already have the composer.sh script installed${COLOUR_OFF}"
 else
   cp composer.sh ~
   chmod +x ~/composer.sh
-  echo "alias composer='~/composer.sh'" >> ~/.zshrc
-  INSTALLED=true
+  if [ -x "$COMPOSER_FILE" ]; then
+    echo "alias composer='~/composer.sh'" >> ~/.zshrc
+    INSTALLED=true
+    echo "${GREEN}Composer: composer.sh installed successfully${COLOUR_OFF}"
+  else
+    ERROR=true
+    echo "${RED}Composer: Error unable to make composer.sh executable${COLOUR_OFF}"
+  fi
 fi
+
+echo "PHP: Attempting to install the php.sh helper script"
 
 PHP_FILE=~/php.sh
 
 if [ -f "$PHP_FILE" ]; then
-  echo "Looks like you already have the php script installed"
+  echo "${YELLOW}PHP: Looks like you already have the php.sh script installed${COLOUR_OFF}"
 else
   cp php.sh ~
   chmod +x ~/php.sh
-  echo "alias php='~/php.sh'" >> ~/.zshrc
-  INSTALLED=true
+  if [ -x "$PHP_FILE" ]; then
+    echo "alias php='~/php.sh'" >> ~/.zshrc
+    INSTALLED=true
+    echo "${GREEN}PHP: php.sh installed successfully${COLOUR_OFF}"
+  else
+    ERROR=true
+    echo "${RED}PHP: Error unable to make php.sh executable${COLOUR_OFF}"
+  fi
 fi
 
+# If any of the installs ran, source ~/.zshrc
 if $INSTALLED; then
+    echo "Sourcing .zshrc"
     source ~/.zshrc
-    clear
-    echo "Install completed"
+    echo "${BOLD_GREEN}Install complete${COLOUR_OFF}"
 else
-  echo "No install needed"
+  if $ERROR; then
+    echo "${BOLD_RED}Error: Install failed${COLOUR_OFF}"
+  else
+    echo "${BOLD_YELLOW}Install already complete${COLOUR_OFF}"
+  fi
 fi
