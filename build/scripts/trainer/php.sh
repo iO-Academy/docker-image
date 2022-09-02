01980e6077669a2a32fe0d344805b189a620b9da
Ashley Coles <ashley@acoles.co.uk>
2022-09-02 11:39:53 +0100

    Fixing issue with running php local server

--- build/scripts/trainer/php.sh
+++ build/scripts/trainer/php.sh
@@ -5,11 +5,9 @@ WD=$(pwd)
 DOCKER_PATH=$(sed 's/^.*\(html.*\).*$/\1/' <<<"$WD")
 FOLDER_CHECK='html'
 
-RED='\033[0;31m'
-COLOUR_OFF='\033[0m'
-
 if [[ "$DOCKER_PATH" == *"$FOLDER_CHECK"* ]]; then
-  docker exec -ti -w "/var/www/$DOCKER_PATH" academy-php php "$*"
+  DOCKER_COMMAND="docker exec -ti -w /var/www/$DOCKER_PATH academy-php php $*"
+  eval $DOCKER_COMMAND
 else
   echo "Local version"
   php "$*"
