081453710c2e0d255fbd79cd699d9910b4f84762
Ashley Coles <ashley@acoles.co.uk>
2023-06-01 10:20:31 +0100

    Update docker-destroy-all.sh to remove sh files

--- docker-destroy-all.sh
+++ docker-destroy-all.sh
@@ -15,5 +15,23 @@ if [ -n "$images" ]; then
         docker rmi -f $images
 fi
 
+phpfile='~/php.sh'
+if [ -f $phpfile ]; then
+   rm "$phpfile"
+   
+fi
+
+composerfile='~/composer.sh'
+if [ -f $composerfile ]; then
+   rm "$composerfile"
+fi
+
+phpunitfile='~/phpunit.sh'
+if [ -f $phpunitfile ]; then
+   rm "$phpunitfile"
+fi
+
+# TODO: Figure out how to remove aliases as well
+
 # Save as docker-destroy-all.sh
 # Run in academyServer directory with ./docker-destroy-all.sh
