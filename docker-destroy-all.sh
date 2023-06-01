1f3872e25eba892c2901fff9db23581c79fa0215
Ashley Coles <ashley@acoles.co.uk>
2023-06-01 11:43:53 +0100

    Update docker-destroy-all.sh

--- docker-destroy-all.sh
+++ docker-destroy-all.sh
@@ -15,23 +15,44 @@ if [ -n "$images" ]; then
         docker rmi -f $images
 fi
 
-phpfile='~/php.sh'
-if [ -f $phpfile ]; then
-   rm "$phpfile"
+# Delete shell script files
+PHP_FILE=~/php.sh
+if [ -f "$PHP_FILE" ]; then
+   rm ~/php.sh
    
 fi
 
-composerfile='~/composer.sh'
-if [ -f $composerfile ]; then
-   rm "$composerfile"
+COMPOSER_FILE=~/composer.sh
+if [ -f "$COMPOSER_FILE" ]; then
+   rm ~/composer.sh
 fi
 
-phpunitfile='~/phpunit.sh'
-if [ -f $phpunitfile ]; then
-   rm "$phpunitfile"
+phpunitfile=~/phpunit.sh
+if [ -f "$phpunitfile" ]; then
+   rm ~/phpunit.sh
 fi
 
-# TODO: Figure out how to remove aliases as well
+# Remove aliases
+cp ~/.zshrc ~/.zshrc_backup
+echo "Backup of zshrc created: ~/.zshrc_backup"
+
+sed "/alias php='~\/php.sh'/d" ~/.zshrc > ~/.zshrc_new
+rm ~/.zshrc
+mv ~/.zshrc_new ~/.zshrc
+echo "PHP alias removed"
+
+sed "/alias composer='~\/composer.sh'/d" ~/.zshrc > ~/.zshrc_new
+rm ~/.zshrc
+mv ~/.zshrc_new ~/.zshrc
+echo "Composer alias removed"
+
+sed "/alias phpunit='~\/phpunit.sh'/d" ~/.zshrc > ~/.zshrc_new
+rm ~/.zshrc
+mv ~/.zshrc_new ~/.zshrc
+echo "PHPUnit alias removed"
+
+# Reload the config
+exec /bin/zsh
 
 # Save as docker-destroy-all.sh
 # Run in academyServer directory with ./docker-destroy-all.sh
