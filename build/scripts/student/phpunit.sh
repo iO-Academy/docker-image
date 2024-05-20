a70a0773905e35727e7b09700b876c3bb5978c3c
Ashley Coles <ashley@acoles.co.uk>
2024-05-20 11:19:59 +0100

    Fixing spaces in docker filepaths

--- build/scripts/student/phpunit.sh
+++ build/scripts/student/phpunit.sh
@@ -9,8 +9,8 @@ RED='\033[0;31m'
 COLOUR_OFF='\033[0m'
 
 if [[ "$DOCKER_PATH" == *"$FOLDER_CHECK"* ]]; then
-  DOCKER_COMMAND="docker exec -w /var/www/$DOCKER_PATH academy-php phpunit $*"
-  eval $DOCKER_COMMAND
+  DOCKER_COMMAND="docker exec -w \"/var/www/$DOCKER_PATH\" academy-php phpunit \"$@\""
+  eval "$DOCKER_COMMAND"
 else
   echo "${RED}Error: You must be in the html folder to run this command${COLOUR_OFF}"
 fi
